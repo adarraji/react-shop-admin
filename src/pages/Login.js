ade30037a95d8e420a33393dd682998cb5742ecd
adarraji <a.darraji@outlook.com>
2022-09-21 21:18:25 -0400

    added login components

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,32 @@
-import React from "react"
+import React from "react";
+import styled from "styled-components";
 
 const Login = () => {
     return (
-        <div>Login</div>
+        <Container>
+            <Input type="text" placeholder="username"></Input>
+            <Input type="password" placeholder="password"></Input>
+            <Button>Login</Button>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;   
+`;
+
+const Input = styled.input`
+   padding: 10px;
+   margin-bottom: 20px;
+`;
+
+const Button = styled.button`
+   padding: 10px;
+   width: 100px;
+`;
 
 export default Login
