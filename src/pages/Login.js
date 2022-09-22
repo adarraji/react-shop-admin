75cb7e46e10cb9ff04e91ef9d339264e12d6a08a
adarraji <a.darraji@outlook.com>
2022-09-22 11:32:12 -0400

    fix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
     const handleClick = (e) => {
         e.preventDefault();
         login(dispatch, { username: username, password: password })
-    }
+    };
 
     return (
         <Container>
