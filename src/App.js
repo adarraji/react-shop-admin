204ca1fb451392bcf01eb55497022808d8ae2e26
Ali Darraji <techbrain203@outlook.com>
2022-09-22 19:11:09 -0400

    allow access only if user is admin

--- src/App.js
+++ src/App.js
@@ -6,25 +6,30 @@ import SinglePage from './pages/SinglePage';
 import NewPage from './pages/NewPage';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { userInputs, productInputs } from "./formSource";
+import { useSelector } from 'react-redux';
 
 function App() {
+  const admin = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.isAdmin
   return (
     <Router>
       <Routes>
-        <Route path="/">
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="users">
-            <Route index element={<List />} />
-            <Route path=":userId" element={<SinglePage />} />
-            <Route path="new" element={<NewPage inputs={userInputs} title="Add New User" />} />
+        <Route path="/login" element={<Login />} />
+        {admin && (
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<SinglePage />} />
+              <Route path="new" element={<NewPage inputs={userInputs} title="Add New User" />} />
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<SinglePage />} />
+              <Route path="new" element={<NewPage inputs={productInputs} title="Add New Product" />} />
+            </Route>
           </Route>
-          <Route path="products">
-            <Route index element={<List />} />
-            <Route path=":productId" element={<SinglePage />} />
-            <Route path="new" element={<NewPage inputs={productInputs} title="Add New Product" />} />
-          </Route>
-        </Route>
+        )
+        }
       </Routes>
     </Router>
   );
